9fef5953069c10793c3af0151730303b75b549a2
Kevin Schneider <schneider.kev@outlook.de>
2024-08-29 11:50:04 +0200

    Wip: move to .net8, add ArcPrototype as submodule

---
+++ tests/ReferenceObjects.fs
@@ -0,0 +1,15 @@
+﻿module ReferenceObjects
+
+open TestUtils
+
+open System.IO
+
+let expected_prototype_commit_hash = "70a7c83e7858a974bf913de2e27d8e44191fc73f"
+
+module ArcPrototype =
+    
+    let arc_summary = ""
+
+    let isa_json = ""
+
+    let arc_ro_crate_metadata = ""
---
+++ tests/TestUtils.fs
@@ -0,0 +1,16 @@
+﻿module TestUtils
+
+open System
+open System.IO
+open type System.Environment
+open Fake.Core
+
+let runTool (tool: string) (args: string []) (dir:string) =
+    CreateProcess.fromRawCommand tool args
+    |> CreateProcess.withWorkingDirectory dir
+    |> CreateProcess.redirectOutput
+    |> Proc.run
+
+let getCommitHash path = 
+    let output = runTool "git" [|"rev-parse"; "HEAD"|] path
+    output
