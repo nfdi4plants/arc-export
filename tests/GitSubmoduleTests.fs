83b9f39d56e165fc2e20cacdaae042a4bd100ea1
Heinrich Lukas Weil <hl.weil@t-online.de>
2025-03-13 21:23:56 +0100

    try test fixture stuff

--- tests/GitSubmoduleTests.fs
+++ tests/GitSubmoduleTests.fs
@@ -12,7 +12,20 @@ let ``ArcPrototype commit hash is correct`` () =
     )
 
 [<Fact>]
+let ``UnregisteredAssay commit hash is correct`` () = 
+    Assert.Equal(
+        ReferenceObjects.expected_unregistedAssay_commit_hash, 
+        TestObjects.unregisteredAssayCommitHash
+    )
+
+[<Fact>]
 let ``Fixtures exist for ArcPrototype commit hash`` () = 
     Assert.True (File.Exists $"fixtures/arc_ro_crate_metadata/ArcPrototype@{TestObjects.prototypeCommitHash}.json", $"arc_ro_crate_metadata file fixture does not exist for {TestObjects.prototypeCommitHash}")
     Assert.True (File.Exists $"fixtures/arc_summary/ArcPrototype@{TestObjects.prototypeCommitHash}.md", $"arc_summary file fixture does not exist for {TestObjects.prototypeCommitHash}")
     Assert.True (File.Exists $"fixtures/isa_json/ArcPrototype@{TestObjects.prototypeCommitHash}.json", $"isa_json file fixture does not exist for {TestObjects.prototypeCommitHash}")
+
+[<Fact>]
+let ``Fixtures exist for UnregisteredAssay commit hash`` () = 
+    Assert.True (File.Exists $"fixtures/arc_ro_crate_metadata/ARC-Export-TestFixture@{TestObjects.unregisteredAssayCommitHash}.json", $"arc_ro_crate_metadata file fixture does not exist for {TestObjects.unregisteredAssayCommitHash}")
+    Assert.True (File.Exists $"fixtures/arc_summary/ARC-Export-TestFixture@{TestObjects.unregisteredAssayCommitHash}.md", $"arc_summary file fixture does not exist for {TestObjects.unregisteredAssayCommitHash}")
+    Assert.True (File.Exists $"fixtures/isa_json/ARC-Export-TestFixture@{TestObjects.unregisteredAssayCommitHash}.json", $"isa_json file fixture does not exist for {TestObjects.unregisteredAssayCommitHash}")
---
+++ tests/fixtures/arc_summary/ARC-Export-TestFixture@787564f36849e4e673c0967fa550b936510efefc.md
@@ -0,0 +1,18 @@
+## [Data set] Untitled ARC
+
+### Registered ARC content:
+
+- root
+    - README.md
+    - isa.investigation.xlsx
+    - assays
+        - SharedSampleWithStudy
+            - README.md
+            - isa.assay.xlsx
+        - UnrelatedAssay
+            - README.md
+            - isa.assay.xlsx
+    - studies
+        - Study1
+            - README.md
+            - isa.study.xlsx
